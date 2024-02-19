1ce79af0a6ee38bc02485c36a8b97331cf65ab61
Rajeeb Shakya <rajshakya2000@gmail.com>
2024-02-19 10:40:38 +0545

    Update createuser.sh
    create predefined user when build

--- build/mysql/init/createuser.sh
+++ build/mysql/init/createuser.sh
@@ -1,1 +1,10 @@
 
+#!/usr/bin/env bash
+
+mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS testing;
+    CREATE USER '$MYSQL_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_USER_PASSWORD';
+    GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
+    ALTER user 'root'@'%' identified with mysql_native_password by '$MYSQL_ROOT_PASSWORD';
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE//_/\\_}\`.* TO '$MYSQL_USER'@'%' ;
+EOSQL
